f1f4e54557cc76064578fd0c761b648e0b5ca6ec
Adrian Tran <38680352+viper12358@users.noreply.github.com>
2020-02-28 23:53:23 -0700

    Update vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #define SIZE 100
-int main(){
+// program to encrypt user string by keyword entered in command line
 
+int main(int argc, char argv[]){
+    // you can define your own plaintext here.
+    // or just do a function call, doesn't matter.
     char plaintext[SIZE];
     char ciphertext[SIZE];
     // this is just an example key. Make one yourself if you want.
@@ -12,44 +15,62 @@ int main(){
     char key[5] = "VIPER";          
     char newKey[SIZE];
     
-    // you can define your own plaintext here.
-    // or just do a function call, doesn't matter.
-
-    // generating the key
-    // i to keep track of the indices in the string, j is for the original key
-    // NOTE: you can hardcode strlen(key) to be exactly 5. Sometimes C compiler will count the the null character,
-    // which result in wrong key output
+    // remember to set the plaintext first, otherwise comment this line out
+    printf("Original plaintext: %s\n", plaintext);
+    
+    /* generating the key
+     i to keep track of the indices in the string, j is for the original key
+     now I have to hard code j == 5 instead of j == strlen(key) since there is 
+     a known problem in Windows computer that run Cygwin:
+     strlen() will count null character '\0' as an extra character. 
+     If you are using Linux/UNIX like I should have, replace 5 with strlen(key)  */
+    
     for (int i = 0, j = 0; i < strlen(plaintext); i++, j++){
-    if (isalpha(plaintext[i])){       // alphabet character
-      if (j == strlen(key)){                    // reach the end of key, reset counter
-        j = 0;   
-      }  
-      newKey[i] = key[j];   
-    }
+        if (isalpha(plaintext[i])){         // if this part of plaintext is an alphabet character
+            if (j == 5){                    // reach the end of key, reset counter
+                j = 0;   
+            }  
+            newKey[i] = key[j];   
+        }
 
-    else {      
-      if (j == strlen(key)){                               
-        j = 0;    
-      }
-      newKey[i] = ' ';    // simply ignore it
-      j-=1;
+        else {      
+            if (j == 5){                   
+                j = 0;    
+            }
+            newKey[i] = ' ';    // simply ignore it (not a alphabet character)
+            j-=1;
+        }
     }
-  }
   
-  // encryption
-  for(int i = 0; i < strlen(plaintext); i++){
-    if (isalpha(newKey[i])){
-      char alpha = islower(plaintext[i]) ? 'a' : 'A';         
-      ciphertext[i] = ((toupper(plaintext[i]) + newKey[i]) % 26) + alpha;   // Vigenere cipher formula
+    printf("Key generate: %s\n", newKey);
+
+    // encryption
+    for(int i = 0; i < strlen(plaintext); i++){
+        if (isalpha(newKey[i])){
+            char alpha = islower(plaintext[i]) ? 'a' : 'A';   // offset by upper case character since we want the output to be uppercase
+            ciphertext[i] = ((toupper(plaintext[i]) + newKey[i]) % 26) + alpha;   // Vigenere cipher formula
+        }
+
+        else {
+            ciphertext[i] = plaintext[i];      //not alphabet => no need for encryption
+        }
     }
 
-    else {
-      ciphertext[i] = plaintext[i];      //not alphabet => no need for encryption
-    }
-  }
+    printf("Cipher text: %s\n", ciphertext);
 
-  // if you want to decrypt, formula is on Wiki, just reverse the bloody process
-  
+    // decryption:
+    
+    for(int i = 0; i < strlen(ciphertext); i ++){
+        if(isalpha(plaintext[i])){
+            char alpha = islower(plaintext[i]) ? 'a' : 'A';
+            plaintext[i] = ((toupper(ciphertext[i]) - newKey[i] + 26) % 26) + alpha;    //adding 26 to get back the correct ASCII value
+        }
+      
+        else {
+            plaintext[i] = ciphertext[i];
+        }
+    }
+
+    printf("Plaintext: %s\n", plaintext);
     return 0;
 }
-  
